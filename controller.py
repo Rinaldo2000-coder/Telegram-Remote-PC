e9958fbf0ba7b7301375c3d033a481dad336b814
Rinaldo <nikilson30@gmail.com>
2022-01-07 15:18:43 +0530

    Buttons added

--- controller.py
+++ controller.py
@@ -1,28 +1,7 @@
-import selenium as sl
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from whatkit import playonyt
 import time, pyautogui
-path = r'C:\Program Files (x86)\chrome_driver\90\chromedriver.exe'
-path1 = r'C:\Program Files (x86)\chrome_driver\edge90\msedgedriver.exe'
-options = Options()
-options.add_argument('start_maximized')
-options.add_experimental_option("useAutomationExtension", False)
-options.add_argument('--disable-infobars')
-driver = webdriver.Chrome(options = options, executable_path = path)
-pyautogui.click(1035, 115)
-pyautogui.moveTo(500,500)
-p144="//span[contains(string(),'144p')]"
-p240="//span[contains(string(),'240p')]"
-p360="//span[contains(string(),'360p')]"
-p720="//span[contains(string(),'720p')]"
-p1080="//span[contains(string(),'1080p')]"
+import webbrowser as web
+from whatkit import playonyt
 
-def MaxWindow(driver =driver):
-    try:
-        driver.maximize_window()
-    except:
-        pass
 def overlayadd():
     pyautogui.click(904, 645)
 def skipAdd():
@@ -50,55 +29,13 @@ def forward_to(inp):
 	x_co = 1315 * num
 	pyautogui.click(x_co, 710)
 	pyautogui.moveTo(500,500)
-def Quality(pixl, driver = driver):
-    try:
-        driver.find_element_by_css_selector('button.ytp-button.ytp-settings-button').click()
-        driver.find_element_by_xpath("//div[contains(text(),'Quality')]").click()
-        quality = driver.find_element_by_xpath(pixl)
-        quality.click()
-    except:
-        pass
-def YTvideo(song, driver = driver, pixl = p1080):
+
+def YTvideo(song):
     song = (song.lower()).replace("play","")
     link = playonyt(song)
-    driver.get(link)
-    Quality(pixl)
-    try:
-        driver.fullscreen_window()
-    except:
-        pass
-def pause_play(driver = driver):
-	for i in range(50):
-	    try:
-	        pause_xpath = '//*[@id="movie_player"]/div['+ str(i) +']/div[2]/div[1]/button'
-	        pause = driver.find_element_by_xpath(pause_xpath)
-	        pause.click()
-	        break
-	    except:
-	    	pass
-def full_screen(driver = driver):
-	status = ""
-	for i in range(50):
-	    try:
-	        flu_xpath = '//*[@id="movie_player"]/div['+ str(i) +']/div[2]/div[2]/button[10]'
-	        driver.find_element_by_xpath(flu_xpath).click()
-	        break
-	    except:
-	        pass
-	    if i == 49:
-	    	status = "failed"
-	if status == "failed":
-		driver.fullscreen_window()
-def Quit(driver = driver):
-    driver.quit()
+    web.open(link)
+
+
 
 if __name__ =='__main__':
     YTvideo("play tech with tim")
-    time.sleep(30)
-    full_screen()
-    time.sleep(5)
-    full_screen()
-    time.sleep(5)
-    pause_play()
-    time.sleep(5)
-    pause_play()
--- main.py
+++ main.py
@@ -1,101 +1,105 @@
 from telegram.ext import *
 import os, pyautogui
 from controller import *
-API_KEY = '1743814674:AAGTkcpXmjnGGGANRegfucaGjPfM7WCDrlA'
-helptext = "You can try\n\n1)Play Vaathi Coming\n2)Change resolution to 1080p\n3)Pause Video\n4)Quit\n5)Shut down\n6)Exit fullscreen"
-helptext = helptext + "\n7)Volume Up\n8)FastF forward to 60%\n9)Skip ad\n10)Close ad"
-p144="//span[contains(string(),'144p')]"
-p240="//span[contains(string(),'240p')]"
-p360="//span[contains(string(),'360p')]"
-p720="//span[contains(string(),'720p')]"
-p1080="//span[contains(string(),'1080p')]"
+from telegram import *
 
-def start(update, content):
-    update.message.reply_text(helptext)
-    update.message.reply_text('Type something...')
-def help1(update, content):
-    update.message.reply_text(helptext)
-def ShutDown(time1 = 20):
-	cont = "shutdown -s -t %s" % time1
-	os.system(cont)
-	ans =  "Your PC will be turned off within 20 Seconds, 'Stop Shutdown' to Stop the shutdown"
-	return ans
-def CancelShutDown():
-	cont = "shutdown /a"
-	os.system(cont)
-def center(update, content):
-	text = str(update.message.text).lower()
-	if ("shutdown") == text:
-		text = str(update.message.text).lower()
-		sts = ShutDown()
-		update.message.reply_text(sts)
-	if ("shut down") == text:
-		text = str(update.message.text).lower()
-		sts = ShutDown()
-		update.message.reply_text(sts)
-	elif "quit" == text:
-		Quit()
-		quit()
-	elif ("play") == text:
-		pause_play()
-	elif "volume" in text:
-		if "down" in text:
-			pyautogui.press('volumedown')
-			pyautogui.press('volumedown')
-			pyautogui.press('volumedown')
-		elif "up" in text:
-			pyautogui.press('volumeup')
-			pyautogui.press('volumeup')
-			pyautogui.press('volumeup')
-	elif ("fast forward") in text:
-		forward_to(text)
-	elif ("back forward") in text:
-		forward_to(text)
-	elif ("pause") == text:
-		pause_play()
-	elif ("play" in text) and ("play" != text):
-		YTvideo(text)
-	elif "change resolution" in text:
-		if "1080" in text:
-			Quality(pixl = p1080)
-		elif "720" in text:
-			Quality(pixl = p720)
-		elif "480" in text:
-			Quality(pixl = p480)
-		elif "360" in text:
-			Quality(pixl = p360)
-		elif "240" in text:
-			Quality(pixl = p240)
-		else:
-			Quality(pixl = p720)
-	elif ("maximize" and "window") in text:
-		MaxWindow()
-	elif ("fullscreen") in text:
-		full_screen()
-	elif ("full screen") in text:
-		full_screen()
-	elif ("skip trial") == text:
-		pyautogui.click(200 ,710)
-		pyautogui.moveTo(500, 500)
-	elif text in ("close", "close ad", "close add"):
-		overlayadd()
-	elif text in ("skip", "skip ad", "skip add"):
-		skipAdd()
-	elif ("cancel" and "shutdown") in text:
-		CancelShutDown()
-		update.message.reply_text("Okay, ShutDown cancelled!")
-	elif ("stop" and "shutdown") in text:
-		CancelShutDown()
-		update.message.reply_text("Okay, ShutDown cancelled!")
-	else:
-		update.message.reply_text("Sorry, I can't do that!")		
-def main(key = API_KEY):
-    updater = Updater(key, use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help1))
-    dp.add_handler(MessageHandler(Filters.text, center))
-    updater.start_polling()
-    updater.idle()
-main()
+class TelegramPcController():
+
+    API_KEY = '1743814674:AAGTkcpXmjnGGGANRegfucaGjPfM7WCDrlA'
+    helptext = "You can try\n\n1)Play Sweet but psycho\n2)Change resolution to 1080p\n3)Pause Video\n4)Quit\n5)Shut down\n6)Exit fullscreen"
+    helptext = helptext + "\n7)Volume Up\n8)Fast forward to 60%\n"
+
+    def start(self, update, content):
+        update.message.reply_text(self.helptext)
+        self.InlineButton(update, content)
+        update.message.reply_text('Type something...')
+
+    def help1(self, update, content):
+        update.message.reply_text(self.helptext)
+
+    def InlineButton(self, update, content):
+        button_text = ["Pause", "Play", "Shutdown", "Cancel Shutdown", "Quit"]
+        button_reply = ["pause", "play", "shutdown", "cancel shutdown", "quit"]
+        buttons = []
+        for i in range(len(button_text)):
+            btn = [InlineKeyboardButton(button_text[i], callback_data=button_reply[i])]
+            buttons.append(btn)
+        content.bot.send_message(chat_id=update.effective_chat.id, reply_markup=InlineKeyboardMarkup(buttons), text="Welcome")
+
+    def Query_Handler(self, update, content):
+        try:
+            self.text = update.callback_query.data
+            # print(self.text)
+            self.CommandCenter(update, content)
+            update.callback_query.answer()     
+        except Exception as e:
+            print(e)
+
+    def ShutDown(self, time1 = 20):
+        cont = "shutdown -s -t %s" % time1
+        os.system(cont)
+        ans =  "Your PC will be turned off within 20 Seconds, 'Stop Shutdown' to Stop the shutdown"
+        return ans
+    def CancelShutDown(self):
+        cont = "shutdown /a"
+        os.system(cont)
+
+    def CommandCenter(self, update, content):
+        text = self.text
+        if ("shutdown" or "shut down") == text:
+            text = str(update.message.text).lower()
+            sts = ShutDown()
+            update.message.reply_text(sts)
+        elif "quit" == text:
+            quit()
+        elif ("play") == text:
+            pyautogui.press("space")
+        elif "volume" in text:
+            if "down" in text:
+                pyautogui.press('volumedown')
+                pyautogui.press('volumedown')
+                pyautogui.press('volumedown')
+            elif "up" in text:
+                pyautogui.press('volumeup')
+                pyautogui.press('volumeup')
+                pyautogui.press('volumeup')
+        elif ("fast forward") in text:
+            forward_to(text)
+        elif ("back forward") in text:
+            forward_to(text)
+        elif ("pause") == text:
+            pyautogui.press("space")
+        elif ("play" in text) and ("play" != text):
+            YTvideo(text)
+        elif "change resolution" in text:
+            pass
+        elif ("maximize" and "window") in text:
+            MaxWindow()
+        elif ("fullscreen" in text ) or ("full screen"in text):
+            pyautogui.press("f")
+        elif ("cancel" and "shutdown") in text:
+            CancelShutDown()
+            update.message.reply_text("Okay, ShutDown cancelled!")
+        elif ("stop" and "shutdown") in text:
+            CancelShutDown()
+            update.message.reply_text("Okay, ShutDown cancelled!")
+        else:
+            update.message.reply_text("Sorry, I can't do that!") 
+    
+    def message_center(self, update, content):
+        self.text = str(update.message.text).lower()
+        self.CommandCenter()
+
+    def main(self, key = API_KEY):
+        updater = Updater(key, use_context=True)
+        dp = updater.dispatcher
+        dp.add_handler(CommandHandler("start", self.start))
+        dp.add_handler(CommandHandler("help", self.help1))
+        dp.add_handler(MessageHandler(Filters.text, self.message_center))
+        dp.add_handler(CallbackQueryHandler(self.Query_Handler))
+        updater.start_polling()
+        updater.idle()
+if __name__ == '__main__':
+    TelegramPcController().main()
+
 
